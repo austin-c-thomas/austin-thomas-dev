65804477960e72b13aa688579df8492b7dbec7ee
Austin <austin.thomas130@gmail.com>
2021-05-18 20:18:37 -0700

    Added scroll link to logo button on header

--- components/header/LogoButton.js
+++ components/header/LogoButton.js
@@ -1,10 +1,15 @@
-import Link from 'next/link';
+import { Link } from 'react-scroll';
 
 const LogoButton = () => {
   return (
     <div className='logo-button'>
 
-      <Link href='/'>
+        <Link 
+            to="intro-block" 
+            spy={true} 
+            smooth={true} 
+            // onClick={() => handleLinkClick("Home")}
+            >
         <>
           <h1>AUSTIN THOMAS</h1>
           <div className='sub-head'>
