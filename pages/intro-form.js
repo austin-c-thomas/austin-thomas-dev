4f824e82d5c21b4dc4b44ce875a61791b94dd099
Austin <austin.thomas130@gmail.com>
2021-06-08 14:10:33 -0700

    Fixed dynamic text top property for mobile

---
+++ pages/intro-form.js
@@ -0,0 +1,39 @@
+// import { TextField, Button, OutlinedInput, InputLabel, FormControl } from "@material-ui/core"
+
+// const defaultData = {
+//   name: '',
+//   email: '',
+//   businessName: '',
+//   websiteStatus
+// }
+
+// const IntroForm = () => {
+//   return (
+//     <>
+//       <main id="intro-form-wrapper">
+//         <h1>Intro Form</h1>
+//         <form id="intro-form">
+//           <TextField 
+//             variant="outlined"
+//             id="name"
+//             label="Name"
+//             required={true}
+//             value={values.name}
+//             onChange={handleInputChange}/>
+//         </form>
+//       </main>
+
+//       <style jsx>
+//         {`
+//           #intro-form-wrapper {
+//             padding-top: 80px;
+//             background-color: #000;
+//             color: #fff;
+//           }
+//         `}
+//       </style>
+//     </>
+//   );
+// };
+
+// export default IntroForm;
